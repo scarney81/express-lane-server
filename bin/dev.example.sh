ed45590615b7dc7cb77037fb64d2c26a7b55fa7c
Seth Carney <seth.carney@eloqua.com>
2012-04-02 18:30:31 -0400

    add shell script for startup. update gitignore

---
+++ bin/dev.example.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+export PORT=3000
+export MONGOLAB_URI=mongodb://<user>:<password>@ds031567.mongolab.com:31567/heroku_app3627039
+
+node app.js
--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 /*globals process */
 module.exports = {
-	port: (process.env.PORT || 3000)
+	port: process.env.PORT,
+	connectionString: process.env.MONGOLAB_URI
 };
