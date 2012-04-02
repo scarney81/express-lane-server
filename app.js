f3a163deb22763ce80df50dc1c62c47201b6cd31
Seth Carney <seth.carney@eloqua.com>
2012-04-02 16:08:37 -0400

    update app.js with heroku port

--- app.js
+++ app.js
@@ -1,14 +1,9 @@
-
-/**
- * Module dependencies.
- */
-
 var express = require('express')
   , routes = require('./routes');
 
-var app = module.exports = express.createServer();
+var port = (process.env.PORT || 3000);
 
-// Configuration
+var app = module.exports = express.createServer();
 
 app.configure(function(){
   app.set('views', __dirname + '/views');
@@ -27,9 +22,7 @@ app.configure('production', function(){
   app.use(express.errorHandler());
 });
 
-// Routes
-
 app.get('/', routes.index);
 
-app.listen(3000);
+app.listen(port);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
