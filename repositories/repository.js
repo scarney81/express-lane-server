dec8f1d42031943910c8a24bc7e4a79e61b3d724
Seth Carney <seth.carney@eloqua.com>
2012-04-02 21:04:46 -0400

    add id conversion back in

--- repositories/repository.js
+++ repositories/repository.js
@@ -42,7 +42,7 @@ module.exports = {
 		var entities = db.collection(collection);
 		return entities.save(model, function(err, result) {
 	    if (err !== null) return callback(err);
-	    //result._id = result._id.toString();
+	    result._id = result._id.toString();
 	    return callback(null, result);
 	  });
 	},
