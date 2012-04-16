f6110e952d0415e14f1c3eecdd7675f2042ea30a
Seth Carney <seth.carney@eloqua.com>
2012-04-16 18:20:05 -0400

    server changes

--- repositories/products.js
+++ repositories/products.js
@@ -7,7 +7,6 @@ module.exports = function(config) {
   return {
     all: function(callback) { self.repo.all(callback); },
     save: function(model, callback) { self.repo.save(model, callback); },
-    remove: function(id, callback) { self.repo.remove(id, callback); },
     
     single: function(id, callback) {
       self.repo.single(id, function(err, product){
--- routes/products.js
+++ routes/products.js
@@ -17,26 +17,8 @@ module.exports = function(products) {
     all: function(req, res, next) { 
       products.all(respond(res));
     },
-    post: function(req, res, next) {
-      var product = {};
-      for (var i = 0; i < white_list.length; i++) {
-        var key = white_list[i];
-        product[key] = req.body[key];
-      }
-      products.save(product, respond(res));
-    },
     get: function(req, res, next) { 
       res.json(req.product); 
-    },
-    put: function(req, res, next) {
-      for (var i = 0; i < white_list.length; i++) {
-        var key = white_list[i];
-        req.product[key] = req.body[key];
-      }
-      req.product.save(respond(res));
-    },
-    remove: function(req, res, next) {
-      req.product.remove(respond(res));
     },
     addReview: function(req, res, next) {
       var review = {};
